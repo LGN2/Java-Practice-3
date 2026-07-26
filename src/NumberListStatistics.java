4cc4868f5b3add13f08d51c6016d38a69862b195
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-26 15:03:46 +0400

    starting this practice

---
+++ src/NumberListStatistics.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberListStatistics {
+    public static void main(String[] args){
+        List<String> numbers = new ArrayList<>();
+
+    }
+}
