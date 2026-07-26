5d89a5f9ae39216056f01f409624ad567b395623
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-26 12:17:46 +0400

    starting this practice

--- src/TriangleCalculator.java
+++ src/TriangleCalculator.java
@@ -4,11 +4,11 @@ public class TriangleCalculator {
     public static void main(String[] args){
         Double base;
         Double height;
-        Double area;
+        Double area = 0.0;
         Double a;
         Double b;
         Double c;
-        Double perimeter;
+        Double perimeter = 0.0;
         String type;
 
         Scanner sc = new Scanner(System.in);
@@ -18,6 +18,28 @@ public class TriangleCalculator {
         c = sc.nextDouble();
         base = sc.nextDouble();
         height = sc.nextDouble();
-        
+
+         if (a <= 0 || b <= 0|| c <= 0|| base <= 0|| height <= 0){
+             IO.println("Invalid input. All values must be greater than zero.");
+         }else {
+             area = (base * height)/2;
+             perimeter = a + b + c;
+         }
+         if (a == b && b == c){
+             type = "Equilateral";
+         } else if (a == b || a == 3 || b == c) {
+             type = "Isosceles";
+         }else {
+             type = "Scalene";
+         }
+        IO.println("The base: "+base);
+        IO.println("The Height: "+height);
+        IO.println("The Side A: "+a);
+        IO.println("The Side B: "+b);
+        IO.println("The Side C: "+c);
+        IO.println("The Triangle Area is: "+area);
+        IO.println("The Triangle perimeter: "+perimeter);
+        IO.println("The Triangle Type is: "+type);
+
     }
 }
