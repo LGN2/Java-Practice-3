44b878e3a66d600402adfbd2925f4f1f98afc119
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-26 14:22:31 +0400

    starting this practice

--- src/TrapezoidCalculator.java
+++ src/TrapezoidCalculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TrapezoidCalculator {
     public static void main(String[] args){
         Double b1;
@@ -9,6 +11,20 @@ public class TrapezoidCalculator {
         Double perimeter;
         String type;
 
-        
+        Scanner sc = new Scanner(System.in);
+        IO.println("Enter the base 1:");
+        b1 = sc.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        IO.println("Enter the base 2:");
+        b2 = sc.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        IO.println("Enter the leg 1:");
+        l1 = sc.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        IO.println("Enter the leg 2:");
+        l2 = sc.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        IO.println("Enter the Height:");
+        height = sc.nextDouble();
     }
 }
