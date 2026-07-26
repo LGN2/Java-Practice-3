f5cff77f062be69189a47a0cb1ac9ca06c4d3725
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-26 14:20:11 +0400

    starting this practice

--- src/TrapezoidCalculator.java
+++ src/TrapezoidCalculator.java
@@ -1,5 +1,14 @@
 public class TrapezoidCalculator {
     public static void main(String[] args){
+        Double b1;
+        Double b2;
+        Double l1;
+        Double l2;
+        Double height;
+        Double area;
+        Double perimeter;
+        String type;
+
         
     }
 }
