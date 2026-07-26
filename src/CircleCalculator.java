ef2618c56fd7f90daba0dbcb3218d82480a055f7
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-26 12:19:20 +0400

    starting this practice

--- src/CircleCalculator.java
+++ src/CircleCalculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class CircleCalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Double pi = 3.14;
         Double radius;
         Double area = 0.0;
@@ -14,25 +14,25 @@ public class CircleCalculator {
         radius = sc.nextDouble();
 
 
-        if (radius <= 0){
+        if (radius <= 0) {
             IO.println("Invalid radius must be greater than zero.");
-        }else{
+        } else {
             area = pi * (radius * radius);
             circumference = 2 * pi * radius;
         }
-        if (radius < 5){
+        if (radius < 5) {
             classification = "Small Circle";
         } else if (radius <= 15) {
             classification = "Medium Circle";
 
-        }else{
+        } else {
             classification = "Large Circle";
 
         }
 
-        IO.println("Radius: "+radius);
-        IO.println("Area: "+area);
-        IO.println("Circumference: "+circumference);
-        IO.println("Classification: "+classification);
+        IO.println("Radius: " + radius);
+        IO.println("Area: " + area);
+        IO.println("Circumference: " + circumference);
+        IO.println("Classification: " + classification);
     }
 }
