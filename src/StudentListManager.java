de16dc269b5479286e47b62f69cc60fbefbf1b06
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-26 14:41:29 +0400

    starting this practice

--- src/StudentListManager.java
+++ src/StudentListManager.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentListManager {
     public static void main(String[] args){
-        
+        List<String> students = new ArrayList<>();
+
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
+        students.add("Abdulrahman");
     }
 }
