697381935e3893f4b888ffb200bff06a5767e25b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-27 10:50:34 +0400

    starting this practice

--- src/StudentResultAnalyzer.java
+++ src/StudentResultAnalyzer.java
@@ -23,6 +23,7 @@ public class StudentResultAnalyzer {
 
             System.out.println("Invalid marks. Please check the student grades.");
         }
+        totalMarks = calculateTotal(math,english,science);
     }
     public static boolean validateMarks(Integer math,
                                         Integer english,
@@ -31,5 +32,11 @@ public class StudentResultAnalyzer {
         return math >= 0 && math <= 100 &&
                 science >= 0 && science <= 100 &&
                 english >= 0 && english <= 100;
+    }
+    public static Integer calculateTotal(Integer math,
+                                         Integer english,
+                                         Integer science) {
+
+        return math + english + science;
     }
 }
