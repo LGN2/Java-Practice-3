1901a3c76915e0cfa1a9254575b5599e29ec67cc
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-27 09:50:24 +0400

    starting this practice

--- src/StudentResultAnalyzer.java
+++ src/StudentResultAnalyzer.java
@@ -1,5 +1,19 @@
 public class StudentResultAnalyzer {
     public static void main(String[] args){
-        
+        String studentName;
+        Integer math;
+        Integer english;
+        Integer science;
+        Integer totalMarks;
+        Integer subNum;
+        Integer averageMarks;
+        Integer finalGrade;
+        String performanceStatus;
+
+        studentName = "Almajd Salim";
+        math = 98;
+        english = 95;
+        science = 86;
+        subNum = 3;
     }
 }
