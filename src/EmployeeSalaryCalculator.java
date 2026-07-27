a82de3d5549aae534d0ea1d2950b072979797852
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-27 09:28:55 +0400

    starting this practice

--- src/EmployeeSalaryCalculator.java
+++ src/EmployeeSalaryCalculator.java
@@ -5,17 +5,29 @@ public class EmployeeSalaryCalculator {
         Integer bonusAmount;
         Integer deductionAmount;
         Integer netSalary;
+        String salaryType;
 
         employeeName = "John Smith";
         basicSalary = 1200;
         bonusAmount = 250;
         deductionAmount = 100;
+
+        netSalary = calculateNetSalary(basicSalary, bonusAmount, deductionAmount);
     }
     public static Integer calculateNetSalary(Integer basicSalary,
                                             Integer bonus,
                                             Integer deduction) {
 
         return basicSalary + bonus - deduction;
+    }
+    public static Integer checkSalaryStatus(Integer netSalary){
+        if (netSalary < 500){
+            return = "Low Salary";
+        } else if (netSalary <= 1500) {
+            return = "Meduim Salary";
+        }else {
+            return = "High Salary";
+        }
     }
 
 }
