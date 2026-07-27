4dde01e075c13db1c5805aa7266403ca01d4a595
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-27 09:15:14 +0400

    starting this practice

--- src/EmployeeSalaryCalculator.java
+++ src/EmployeeSalaryCalculator.java
@@ -1,5 +1,11 @@
 public class EmployeeSalaryCalculator {
     public static void main(String[] args){
+        String employeeName;
+        Integer basicSalary;
+        Integer bonusAmount;
+        Integer deductionAmount;
+        Integer netSalary;
+
         
     }
 }
