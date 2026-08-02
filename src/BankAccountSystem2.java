5c718c78c469efc3f8f03484ec896dc0ddfac45d
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-02 11:41:42 +0400

    starting this practice

--- src/BankAccountSystem2.java
+++ src/BankAccountSystem2.java
@@ -143,4 +143,20 @@ public class BankAccountSystem2 {
 
         sc.close();
     }
+
+    public static void displayAccounts(
+            ArrayList<Integer> accountNumbers,
+            ArrayList<String> customerNames,
+            ArrayList<Double> accountBalances) {
+
+        System.out.println("\n========== ACCOUNT LIST ==========");
+
+        for (Integer i = 0; i < accountNumbers.size(); i++) {
+
+            System.out.println("--------------------------------");
+            System.out.println("Account Number : " + accountNumbers.get(i));
+            System.out.println("Customer Name  : " + customerNames.get(i));
+            System.out.println("Balance        : $" + accountBalances.get(i));
+        }
+    }
 }
