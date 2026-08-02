060610052af7e52d9f86b74c52b801fed7759afa
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-02 11:34:37 +0400

    starting this practice

--- src/BankAccountSystem2.java
+++ src/BankAccountSystem2.java
@@ -1,6 +1,16 @@
 public class BankAccountSystem2 {
 
     public static void main(String[] args){
-        
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> accountNumbers;
+        ArrayList<String> customerNames;
+        ArrayList<Double> accountBalances;
+
+        Integer choice;
+        Integer accountNumber;
+        Double amount;
+
     }
 }
